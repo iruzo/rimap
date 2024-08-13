39797d29a442acb1391073614152cff2d2cba708
ruzo <dev@ruzo.sh>
2024-08-13 19:47:57 +0300

    add: flakes devshell

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Rimap";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      # DevShell for your project
+      devShell = pkgs.mkShell {
+        buildInputs = [
+          pkgs.rustup
+          pkgs.rust-analyzer
+          pkgs.openssl
+          pkgs.git
+        ];
+      };
+
+    });
+}
